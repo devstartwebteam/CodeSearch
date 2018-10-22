10b6db34999dc5046d9e53247c4655fbdf82f0a8
feynmanwebteam <feynmanwebteam@gmail.com>
2018-10-22 13:09:02 -0700

    Add project files.

---
+++ CodeSearch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeSearch.MvcApplication" Language="C#" %>
---
+++ CodeSearch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeSearch.Startup))]
+namespace CodeSearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
