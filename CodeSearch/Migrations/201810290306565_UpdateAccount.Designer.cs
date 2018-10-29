b1c0190cb8d34df3c25d54b24f0ef5cc3ff009e4
Nate <devstartwebteam@gmail.com>
2018-10-28 20:31:04 -0700

    account views updated

--- CodeSearch/Migrations/201810290306565_UpdateAccount.Designer.cs
+++ CodeSearch/Migrations/201810290306565_UpdateAccount.Designer.cs
@@ -7,13 +7,13 @@ namespace CodeSearch.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class SnippetLanguageAdded : IMigrationMetadata
+    public sealed partial class UpdateAccount : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(SnippetLanguageAdded));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(UpdateAccount));
         
         string IMigrationMetadata.Id
         {
-            get { return "201810222034162_SnippetLanguageAdded"; }
+            get { return "201810290306565_UpdateAccount"; }
         }
         
         string IMigrationMetadata.Source
